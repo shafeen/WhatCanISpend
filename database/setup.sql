772af67424ce6fadc26c0995e2bb8a52fb818a25
Shafeen M <sodl02@gmail.com>
2016-05-06 16:15:29 -0400

    updated setup.sql and .gitignore

--- database/setup.sql
+++ database/setup.sql
@@ -10,6 +10,14 @@ CREATE TABLE budgets(
   id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(20) NOT NULL,
   type_id INTEGER NOT NULL,
-  FOREIGN KEY (type_id) REFERENCES budget_types(id)
+  FOREIGN KEY (type_id) REFERENCES budget_types(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE items(
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  budget_id INTEGER NOT NULL,
+  description VARCHAR(25) NOT NULL,
+  duration UNSIGNED INT NOT NULL, -- duration can mean weeks/months/years depending on the budget_type
+  FOREIGN KEY (budget_id) REFERENCES budgets(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
