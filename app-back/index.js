39a326dc474a537d8b0873de19fc9eb18d63514b
Shafeen M <sodl02@gmail.com>
2016-05-07 14:18:46 -0400

    cleaned up and created virtual paths for static files

--- app-back/index.js
+++ app-back/index.js
@@ -1,56 +1,37 @@
+var util = require('./util');
 var express = require('express');
 var app = express();
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.get('/node_modules/bootstrap/dist/js/bootstrap.js', function (req, res) {
-    res.sendfile('node_modules/bootstrap/dist/js/bootstrap.js');
-});
-app.get('/node_modules/bootstrap/dist/css/bootstrap.css', function (req, res) {
-    res.sendfile('node_modules/bootstrap/dist/css/bootstrap.css');
-});
-app.get('/node_modules/bootstrap/dist/css/bootstrap-theme.css', function (req, res) {
-    res.sendfile('node_modules/bootstrap/dist/css/bootstrap-theme.css');
-});
-app.get('/node_modules/jquery/dist/jquery.js', function (req, res) {
-    res.sendfile('node_modules/jquery/dist/jquery.js');
-});
-app.get('/whatCanISpend.jpg', function (req, res) {
-    res.sendfile('whatCanISpend.jpg');
-});
-app.get('/createBudget.png', function (req, res) {
-    res.sendfile('createBudget.png');
-});
-app.get('/listOfBudgets.png', function (req, res) {
-    res.sendfile('listOfBudgets.png');
-});
-
-app.get('/shafeen', function (req, res) {
-  res.send('<b>Hello Shafeen!</b>');
-});
-
-app.get('/budget/monthly/', function (req, res) {
-  res.send('<b>Hello Rashmi!</b>');
-});
-
-app.get('/our/site/', function (req, res) {
-  res.sendfile('index.html');
+  res.send(util.add('Hello again,', 'from the other siiiide!'));
 });
 
 app.get('/createBudget', function (req, res) {
-    res.sendfile('createBudget.html');
+    res.sendFile('createBudget.html', {root: '../app-front/'});
+});
+
+app.get('/home', function (req, res) {
+    res.sendFile('home.html', {root: '../app-front/'});
+});
+
+app.get('/test/', function (req, res) {
+  res.sendFile('index.html', {root: '../app-front/'});
 });
 
 
-app.get('/our/site/again', function (req, res) {
-  res.sendfile('index.html');
-});
-
-app.get('/budget', function (req, res) {
-    res.sendfile('home.html');
-});
+// STATIC STUFF
+app.use('/lib/', express.static('../node_modules/bootstrap/dist/js/'));
+app.use('/lib/', express.static('../node_modules/bootstrap/dist/css/'));
+app.use('/lib/', express.static('../node_modules/jquery/dist/'));
+app.use('/images/', express.static('../app-front/images/'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+
+
+
+
+
+
