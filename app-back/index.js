f1bcdd5257fa9331a68b220d6a5941cc11c08faf
Shafeen M <sodl02@gmail.com>
2016-05-07 22:40:52 -0400

    created api route stubs: /budget/all/ & /budget/create/

--- app-back/index.js
+++ app-back/index.js
@@ -2,20 +2,49 @@ var util = require('./util');
 var express = require('express');
 var app = express();
 
+// using bodyparser for POST body parsing
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', function (req, res) {
-  res.send(util.add('Hello again,', 'from the other siiiide!'));
+    res.redirect('/home');
+    //res.send(util.add('Hello again,', 'from the other siiiide!'));
 });
 
-app.get('/createBudget', function (req, res) {
+app.get('/createBudget/', function (req, res) {
     res.sendFile('createBudget.html', {root: '../app-front/'});
 });
 
-app.get('/home', function (req, res) {
+app.get('/home/', function (req, res) {
     res.sendFile('home.html', {root: '../app-front/'});
 });
 
 app.get('/test/', function (req, res) {
-  res.sendFile('index.html', {root: '../app-front/'});
+    res.sendFile('index.html', {root: '../app-front/'});
+});
+
+
+// THE MAIN ROUTES FOR THE APP
+app.get('/budget/all/', function(req, res) {
+    // TODO: complete this to return info about all available budgets
+    res.json([
+        {message: 'Hello, Earth!'},
+        {message: 'Hello, Mars!'},
+        {message: 'Hello, Venus!'}
+    ]);
+});
+
+app.post('/budget/create/', function (req, res) {
+    // TODO: complete this to create one budget
+    var budgetName = req.body.name;
+    var budgetAmt = parseInt(req.body.amount);
+    if (budgetName != undefined && !Number.isNaN(budgetAmt) && budgetAmt > 0) {
+        res.status(200).send(
+            'Creating budget with\nname: ' + budgetName +
+            '\namount: ' + budgetAmt
+        );
+    }
 });
 
 
@@ -26,7 +55,7 @@ app.use('/lib/', express.static('../node_modules/jquery/dist/'));
 app.use('/images/', express.static('../app-front/images/'));
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
 
 
