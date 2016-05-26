014c28257b59c75ea126ddd2fc51f99b68f21289
Shafeen M <sodl02@gmail.com>
2016-05-26 19:00:47 -0400

    refactored budget.js to use modules this will help keep the global namespace clean

--- app-front/scripts/budget.js
+++ app-front/scripts/budget.js
@@ -1,35 +1,40 @@
 $(document).ready(function () {
-    hideUnusedForms();
-    initClickHandlers();
-
+    budgetPageUtil.hideUnusedForms();
+    budgetPageUtil.initClickHandlers();
 });
 
+var budgetPageUtil = (function($) {
+    function hideUnusedForms() {
+        $('#form-create-budget, #form-add-item').hide();
+    }
 
-function hideUnusedForms() {
-    $('#form-create-budget, #form-add-item').hide();
-}
-
-function initClickHandlers() {
-    $('#budget-create-btn').click(function (e) {
-        $('#form-create-budget').fadeIn();
-    });
-
-    $('#budget-add-item-btn').click(function (e) {
-        $('#item-start-date, #item-end-date' ).datepicker();
-        $('#form-add-item').fadeIn();
-    });
-
-    $('#getItemList').click(function (e) {
-        $.get('/budget/1/info').done(function (budgetInfo) {
-            $('#budgetItemList').append(budgetInfo.budgetName);
-            var listItemTemplate = Handlebars.compile($('#listItemTemplate').html());
-            budgetInfo.items.forEach(function (item) {
-                var $item = $(listItemTemplate(item));
-                $('#budgetItemList').append($item);
-                $item.hide().fadeIn('slow');
-            });
-        }).fail(function () {
-            alert("Couldn't get budget info!");
+    function initClickHandlers() {
+        $('#budget-create-btn').click(function (e) {
+            $('#form-create-budget').fadeIn();
         });
-    });
-}
+
+        $('#budget-add-item-btn').click(function (e) {
+            $('#item-start-date, #item-end-date' ).datepicker();
+            $('#form-add-item').fadeIn();
+        });
+
+        $('#getItemList').click(function (e) {
+            $.get('/budget/1/info').done(function (budgetInfo) {
+                $('#budgetItemList').append(budgetInfo.budgetName);
+                var listItemTemplate = Handlebars.compile($('#listItemTemplate').html());
+                budgetInfo.items.forEach(function (item) {
+                    var $item = $(listItemTemplate(item));
+                    $('#budgetItemList').append($item);
+                    $item.hide().fadeIn('slow');
+                });
+            }).fail(function () {
+                alert("Couldn't get budget info!");
+            });
+        });
+    }
+
+    return {
+        hideUnusedForms: hideUnusedForms,
+        initClickHandlers: initClickHandlers
+    }
+})(jQuery);
