16e730dfee4e6ab2bd63981342bb919178c54aa1
Shafeen M <sodl02@gmail.com>
2016-05-26 00:40:46 -0400

    added form fields to the web interface for use later

--- app-front/scripts/budget.js
+++ app-front/scripts/budget.js
@@ -1,3 +1,35 @@
 $(document).ready(function () {
-    $('#item-start-date, #item-end-date' ).datepicker();
+    hideUnusedForms();
+    initClickHandlers();
+
 });
+
+
+function hideUnusedForms() {
+    $('#form-create-budget, #form-add-item').hide();
+}
+
+function initClickHandlers() {
+    $('#budget-create-btn').click(function (e) {
+        $('#form-create-budget').fadeIn();
+    });
+
+    $('#budget-add-item-btn').click(function (e) {
+        $('#item-start-date, #item-end-date' ).datepicker();
+        $('#form-add-item').fadeIn();
+    });
+
+    $('#getItemList').click(function (e) {
+        $.get('/budget/1/info').done(function (budgetInfo) {
+            $('#budgetItemList').append(budgetInfo.budgetName);
+            var listItemTemplate = Handlebars.compile($('#listItemTemplate').html());
+            budgetInfo.items.forEach(function (item) {
+                var $item = $(listItemTemplate(item));
+                $('#budgetItemList').append($item);
+                $item.hide().fadeIn('slow');
+            });
+        }).fail(function () {
+            alert("Couldn't get budget info!");
+        });
+    });
+}
