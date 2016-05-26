46c106790279a4275e8ca4abdfd8d31e21bc6400
Shafeen M <sodl02@gmail.com>
2016-05-26 19:37:57 -0400

    updated and added form fields for the web interface

--- app-front/scripts/budget.js
+++ app-front/scripts/budget.js
@@ -19,17 +19,31 @@ var budgetPageUtil = (function($) {
         });
 
         $('#getItemList').click(function (e) {
-            $.get('/budget/1/info').done(function (budgetInfo) {
-                $('#budgetItemList').append(budgetInfo.budgetName);
+            var infoApiPath = '/budget/'+$('#budget-id').val()+'/info/';
+            $.get(infoApiPath).done(function (budgetInfo) {
+                var $budgetItemList = $('#budgetItemList').empty();
+                $budgetItemList.append(budgetInfo.budgetName);
                 var listItemTemplate = Handlebars.compile($('#listItemTemplate').html());
                 budgetInfo.items.forEach(function (item) {
                     var $item = $(listItemTemplate(item));
-                    $('#budgetItemList').append($item);
+                    $budgetItemList.append($item);
                     $item.hide().fadeIn('slow');
                 });
             }).fail(function () {
-                alert("Couldn't get budget info!");
+                alert("Couldn't get budget info from: "+infoApiPath);
             });
+        });
+
+        $('#budget-list-all').click(function (e) {
+            $.get('/budget/all/').done(function (budgetList) {
+                var $budgetList = $('#budgetList').empty();
+                budgetList.forEach(function (budgetObj) {
+                    // TODO: update this to a better list (use a template)
+                    $budgetList.append(JSON.stringify(budgetObj)).append('<br/>');
+                });
+            }).fail(function () {
+                alert("Couldn't get budget list!");
+            })
         });
     }
 
