74b6ba6331ab5d7e0d328775ceda24c7ed4efe20
Shafeen M <sodl02@gmail.com>
2017-05-10 23:20:23 -0500

    extracted the old budgetApi into budgetApi_old.js

--- app-back/index.js
+++ app-back/index.js
@@ -1,5 +1,4 @@
 var path = require('path');
-var dbUtil = require('./dbUtil');
 var express = require('express');
 var app = express();
 
@@ -49,112 +48,7 @@ app.get('/test/', function (req, res) {
 
 
 // THE MAIN ROUTES FOR THE API
-
-// [ params ]
-//  name (String),
-//  amount (Number),
-//  type (String)
-app.post('/budget/create/', function (req, res) {
-    console.log('received api request: /budget/create/');
-    var budgetName = req.body.name;
-    var budgetAmt = !isNaN(req.body.amount)? Number(req.body.amount) : null;
-    var budgetType = req.body.type ? req.body.type : 'weekly';
-    if (budgetName != undefined && budgetAmt != null && budgetAmt > 0) {
-        dbUtil.createBudget(budgetName, budgetAmt, budgetType)
-        .then(function (budgetObj) {
-            res.status(201).json({
-                message: 'Successfully created budget: ' + budgetObj.name,
-                budget: budgetObj
-            });
-        }).catch(function (failReasonsObj) {
-            if (failReasonsObj.type) {
-                res.status(400).json({
-                    message: 'Check parameters and try again.',
-                    reasons: failReasonsObj
-                });
-            } else {
-                res.status(500).json({
-                    message: 'Server Error.',
-                    reasons: failReasonsObj
-                });
-            }
-        });
-    } else {
-        res.status(400).json({
-            message: 'Check parameters and try again.',
-            reasons: {
-                name: (budgetName==undefined? 'in':'')+'valid name',
-                amount: ((budgetAmt==null || budgetAmt <=0)? 'in':'')+'valid amount'
-            }
-        });
-    }
-});
-
-app.get('/budget/all/', function (req, res) {
-    console.log('received api request: /budget/all/');
-    dbUtil.getAllBudgets()
-    .then(function (budgetsArray) {
-        res.json(budgetsArray);
-    }).catch(function () {
-        res.status(500).json({
-            message: 'Server error.'
-        });
-    });
-});
-
-// [ params ]
-//  budgetId (int),
-//  name (String),
-//  cost (Number),
-//  endDate (unix timestamp seconds),
-//  startDate (unix timestamp seconds)
-app.post('/budget/additem/', function (req, res) {
-    console.log('received api request: /budget/additem/');
-    var budgetId = req.body.budgetId;
-    var itemName = req.body.name;
-    var itemCost = req.body.cost;
-    var endDate = req.body.endDate;
-    var startDate = req.body.startDate;
-    dbUtil.budgetAddItem(budgetId, itemName, itemCost, endDate, startDate)
-    .then(function (itemObj) {
-        res.status(201).json({
-            message: 'Added item',
-            item: itemObj
-        });
-    }).catch(function (failReasonsObj) {
-        var failStatus = (failReasonsObj.serverError)? 500 : 400;
-        res.status(failStatus).json({
-            message: failStatus==500? 'Server Error' : 'Check params and try again',
-            reason: failReasonsObj
-        });
-    });
-
-});
-
-// [ params ]
-//  budgetId (int)
-app.get('/budget/:id/info/', function (req, res) {
-    console.log('received api request: /budget/:id/info/ (id = %d)', req.params.id);
-    if(!isNaN(req.params.id)) {
-        var budgetId = Number(req.params.id);
-        dbUtil.getBudgetInfo(budgetId)
-        .then(function (budgetInfoObj) {
-            res.json(budgetInfoObj);
-        }).catch(function (failReasonsObj){
-            res.status(500).json({
-                message: 'Server Error',
-                reason: failReasonsObj
-            });
-        });
-    } else {
-        res.status(400).json({
-            message: 'check params and try again',
-            reason: {
-                id: 'invalid'
-            }
-        });
-    }
-});
+app.use('/budget/', require('./routes/budgetApi_old'));
 
 // STATIC STUFF
 app.use('/lib/', express.static('../node_modules/bootstrap/dist/js/'));
